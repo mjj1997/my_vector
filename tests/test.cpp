ce2c478de8212dd984575559448019e9cdbe2284
Ma Jiajun <691460087@qq.com>
2025-04-26 16:44:49 +0800

    test: rewrite unit-tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,117 +1,52 @@
 #include "include/MyVector.h"
 
-#include <exception>
 #include <gtest/gtest.h>
 
-void print(myvector::MyVector& v, const char* msg)
-{
-    std::cout << "The contents of " << msg << " are:";
-    for (int i{ 0 }; i != v.size(); ++i) {
-        std::cout << ' ' << v[i];
-    }
-    std::cout << '\n';
-}
-
-TEST(MyVectorTest, Test1)
+TEST(MyVectorTest, EmptyVector)
 {
     myvector::MyVector v;
-    EXPECT_EQ(v.size(), 0);
 
+    EXPECT_TRUE(v.empty());
+    EXPECT_EQ(v.size(), 0);
+    EXPECT_EQ(v.capacity(), 0);
+}
+
+TEST(MyVectorTest, PushBack)
+{
+    myvector::MyVector v;
     for (int i{ 0 }; i < 10; ++i) {
         v.push_back(i);
     }
-    print(v, "v");
+
     EXPECT_EQ(v.size(), 10);
-
-    for (int i{ 0 }; i < 10; ++i) {
-        EXPECT_EQ(v[i], i);
-    }
-
-    v.clear();
-    EXPECT_TRUE(v.empty());
-    EXPECT_EQ(v.size(), 0);
+    EXPECT_EQ(v.capacity(), 10);
 }
 
-TEST(MyVectorTest, Test2)
+TEST(MyVectorTest, Clear)
+{
+    myvector::MyVector v;
+    v.push_back(1);
+    v.clear();
+
+    EXPECT_TRUE(v.empty());
+    EXPECT_EQ(v.size(), 0);
+    EXPECT_EQ(v.capacity(), 0);
+}
+
+TEST(MyVectorTest, LargeCapacity)
 {
     myvector::MyVector v;
 
-    int n{ 100000 };
+    const int n{ 100000 };
     for (int i{ 0 }; i < n; ++i) {
         v.push_back(i);
     }
+
     EXPECT_EQ(v.size(), n);
+    EXPECT_GE(v.capacity(), n);
 
     for (int i{ 0 }; i < n; i++) {
         EXPECT_EQ(v[i], i);
-    }
-
-    v.clear();
-    EXPECT_TRUE(v.empty());
-    EXPECT_EQ(v.size(), 0);
-}
-
-TEST(MyVectorTest, Test3)
-{
-    myvector::MyVector v1;
-    EXPECT_TRUE(v1.empty());
-    EXPECT_EQ(v1.size(), 0);
-
-    myvector::MyVector v2(4, 100);
-    EXPECT_FALSE(v2.empty());
-    EXPECT_EQ(v2.size(), 4);
-
-    myvector::MyVector v3(v2);
-    EXPECT_FALSE(v3.empty());
-    EXPECT_EQ(v3.size(), v2.size());
-
-    v3.clear();
-    EXPECT_TRUE(v3.empty());
-    EXPECT_EQ(v3.size(), 0);
-
-    v3.push_back(16);
-    v3.push_back(2);
-    v3.push_back(77);
-    v3.push_back(29);
-    EXPECT_FALSE(v3.empty());
-    EXPECT_EQ(v3[0], 16);
-    EXPECT_EQ(v3[3], 29);
-    int myInts[] = { 16, 2, 77, 29 };
-    EXPECT_EQ(v3.size(), sizeof(myInts) / sizeof(int));
-    print(v3, "v3"); //The contents of v3 are:16 2 77 29
-
-    v3.push_back(30);
-    EXPECT_EQ(v3[4], 30);
-    EXPECT_EQ(v3.size(), 5);
-    print(v3, "v3"); //The contents of v3 are:16 2 77 29 30
-    EXPECT_EQ(v3.size(), sizeof(myInts) / sizeof(int) + 1);
-
-    v1 = v3;
-    print(v1, "v1"); //The contents of first are:16 2 77 29 30
-    EXPECT_FALSE(v1.empty());
-    EXPECT_EQ(v1.size(), v3.size());
-}
-
-TEST(MyVectorTest, Test4)
-{
-    myvector::MyVector a;
-    a.push_back(16);
-    a.push_back(2);
-    a.push_back(77);
-    a.push_back(29);
-
-    {
-        myvector::MyVector b(a);
-        b.push_back(2);
-        EXPECT_EQ(b[4], 2);
-        EXPECT_EQ(b[0], 16);
-    }
-
-    {
-        myvector::MyVector c;
-        c = a;
-        c[0] = 1;
-        EXPECT_EQ(c[0], 1);
     }
 }
 
@@ -122,6 +57,96 @@ TEST(MyVectorTest, NegativeN)
     } catch (const std::exception& e) {
         std::cout << e.what() << std::endl;
     }
+}
+
+TEST(MyVectorTest, ReserveSmaller)
+{
+    myvector::MyVector v(10, 0);
+    v.reserve(5);
+    EXPECT_EQ(v.capacity(), 10);
+}
+
+TEST(MyVectorTest, ManualReserve)
+{
+    myvector::MyVector v;
+    v.reserve(20);
+    EXPECT_EQ(v.capacity(), 20);
+}
+
+TEST(MyVectorTest, ZeroSizeConstructor)
+{
+    myvector::MyVector v(0, 0);
+    EXPECT_TRUE(v.empty());
+    EXPECT_EQ(v.size(), 0);
+    EXPECT_EQ(v.capacity(), 0);
+}
+
+TEST(MyVectorTest, AutoResizing)
+{
+    myvector::MyVector v;
+    v.push_back(0);
+
+    for (int i{ 0 }; i < 9; ++i) {
+        v.push_back(i);
+    }
+
+    v.push_back(10);
+    EXPECT_EQ(v.size(), 11);
+    EXPECT_EQ(v.capacity(), 20);
+}
+
+TEST(MyVectorTest, ClearThenFill)
+{
+    myvector::MyVector v;
+    v.push_back(1);
+    v.clear();
+    v.push_back(2);
+
+    EXPECT_EQ(v.size(), 1);
+    EXPECT_EQ(v[0], 2);
+    EXPECT_EQ(v.capacity(), 10);
+}
+
+TEST(MyVectorTest, CopyConstructor)
+{
+    myvector::MyVector originalVec(5, 10);
+    myvector::MyVector otherVec(originalVec);
+
+    EXPECT_EQ(originalVec.size(), otherVec.size());
+    EXPECT_EQ(originalVec.capacity(), otherVec.capacity());
+
+    for (size_t i{ 0 }; i < originalVec.size(); ++i) {
+        EXPECT_EQ(originalVec[i], otherVec[i]);
+    }
+
+    otherVec[0] = 20;
+    EXPECT_EQ(originalVec[0], 10);
+}
+
+TEST(MyVectorTest, CopyAssignment)
+{
+    myvector::MyVector v1(3, 1);
+    myvector::MyVector v2(5, 2);
+    v1 = v2;
+    EXPECT_EQ(v1.capacity(), 5);
+
+    EXPECT_EQ(v1.size(), v2.size());
+    EXPECT_EQ(v1.capacity(), v2.capacity());
+    for (size_t i{ 0 }; i < v1.size(); ++i) {
+        EXPECT_EQ(v1[i], v2[i]);
+    }
+
+    v1[0] = 3;
+    EXPECT_EQ(v2[0], 2);
+}
+
+TEST(MyVectorTest, SelfAssignment)
+{
+    myvector::MyVector v;
+    v.push_back(1);
+    v = v;
+    EXPECT_EQ(v.size(), 1);
+    EXPECT_EQ(v[0], 1);
 }
 
 int main(int argc, char* argv[])
