660c59fb56cc0353eac7c171116a800be8b265c8
Ma Jiajun <691460087@qq.com>
2025-04-27 15:50:23 +0800

    demo: add usage examples

---
+++ examples/iterator_example.cpp
@@ -0,0 +1,23 @@
+#include "include/MyVector.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    myvector::MyVector v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    for (auto it{ v.begin() }; it != v.end(); ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << std::endl;
+
+    for (const auto& val : v) {
+        std::cout << val << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
