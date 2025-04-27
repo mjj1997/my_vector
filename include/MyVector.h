d6a986dfbe38030c041664b78163b2dd5bf430dd
Ma Jiajun <691460087@qq.com>
2025-04-27 18:18:01 +0800

    feat: add template support

--- include/MyVector.h
+++ include/MyVector.h
@@ -1,73 +1,151 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
+#include <stdexcept>
 
 namespace myvector {
 
+template<typename T>
 class MyVector
 {
 public:
-    MyVector();
-    MyVector(int n, int value);
-    ~MyVector();
+    MyVector() {}
+    MyVector(int n, const T& value)
+    {
+        if (n < 0) {
+            throw std::invalid_argument{ "n cannot be negative" };
+        }
 
-    MyVector(const MyVector& other);
-    MyVector& operator=(const MyVector& rhs);
+        reserve(n);
 
-    size_t size() const;
-    size_t capacity() const;
-    bool empty() const;
+        for (int i{ 0 }; i < n; ++i) {
+            m_data[i] = value;
+        }
+
+        m_size = n;
+    }
+
+    ~MyVector() { delete[] m_data; }
+
+    MyVector(const MyVector& other)
+        : m_data{ new T[other.m_capacity] }
+        , m_size{ other.m_size }
+        , m_capacity{ other.m_capacity }
+    {
+        std::copy(other.m_data, other.m_data + other.m_size, m_data);
+    }
+
+    MyVector& operator=(const MyVector& rhs)
+    {
+        if (&rhs == this) {
+            return *this;
+        }
+
+        clear();
+
+        m_data = new T[rhs.m_capacity];
+        m_size = rhs.m_size;
+        m_capacity = rhs.m_capacity;
+
+        std::copy(rhs.m_data, rhs.m_data + rhs.m_size, m_data);
+
+        return *this;
+    }
+
+    size_t size() const { return m_size; }
+    size_t capacity() const { return m_capacity; }
+    bool empty() const { return m_size == 0; }
 
     //只读元素
-    const int& operator[](size_t n) const;
+    const T& operator[](size_t n) const { return m_data[n]; }
     //写入元素
-    int& operator[](size_t n);
+    T& operator[](size_t n) { return m_data[n]; }
 
-    void reserve(size_t newCapacity);
-    void push_back(int value);
-    void clear();
+    void reserve(size_t newCapacity)
+    {
+        if (newCapacity <= m_capacity)
+            return;
+
+        int* newData{ new T[newCapacity] };
+        std::copy(m_data, m_data + m_size, newData);
+        delete[] m_data;
+        m_data = newData;
+        m_capacity = newCapacity;
+    }
+
+    void push_back(const T& value)
+    {
+        if (m_size == m_capacity) {
+            size_t newCapacity{ m_capacity == 0 ? 10 : m_capacity * 2 };
+            reserve(newCapacity);
+        }
+
+        m_data[m_size] = value;
+        ++m_size;
+    }
+
+    void clear()
+    {
+        delete[] m_data;
+        m_data = nullptr;
+        m_size = 0;
+        m_capacity = 0;
+    }
 
     class iterator
     {
     public:
-        iterator(int* ptr);
+        iterator(T* ptr)
+            : m_ptr{ ptr }
+        {}
 
-        int& operator*();
-        iterator& operator++();
+        T& operator*() { return *m_ptr; }
+        iterator& operator++()
+        {
+            ++m_ptr;
+            return *this;
+        }
 
-        bool operator==(const iterator& rhs) const;
-        bool operator!=(const iterator& rhs) const;
+        bool operator==(const iterator& rhs) const { return m_ptr == rhs.m_ptr; }
+        bool operator!=(const iterator& rhs) const { return m_ptr != rhs.m_ptr; }
 
     private:
         friend class MyVector;
 
-        int* m_ptr{ nullptr };
+        T* m_ptr{ nullptr };
     };
 
     class const_iterator
     {
     public:
-        const_iterator(int* ptr);
+        const_iterator(const T* ptr)
+            : m_ptr{ ptr }
+        {}
 
-        const int& operator*() const;
-        const_iterator& operator++();
+        const T& operator*() const { return *m_ptr; }
+        const_iterator& operator++()
+        {
+            ++m_ptr;
+            return *this;
+        }
 
-        bool operator==(const const_iterator& rhs) const;
-        bool operator!=(const const_iterator& rhs) const;
+        bool operator==(const const_iterator& rhs) const { return m_ptr == rhs.m_ptr; }
+        bool operator!=(const const_iterator& rhs) const { return m_ptr != rhs.m_ptr; }
 
     private:
         friend class MyVector;
 
-        const int* m_ptr{ nullptr };
+        const T* m_ptr{ nullptr };
     };
 
-    iterator begin() noexcept;
-    iterator end() noexcept;
-    const_iterator cbegin() const noexcept;
-    const_iterator cend() const noexcept;
+    iterator begin() noexcept { return iterator{ m_data }; }
+    iterator end() noexcept { return iterator{ m_data + m_size }; }
+    const_iterator cbegin() const noexcept { return const_iterator{ m_data }; }
+    const_iterator cend() const noexcept { return const_iterator{ m_data + m_size }; }
 
 private:
-    int* m_data{ nullptr };
+    T* m_data{ nullptr };
     size_t m_size{ 0 };
     size_t m_capacity{ 0 };
 };
